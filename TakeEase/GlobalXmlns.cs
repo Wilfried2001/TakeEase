3d9f3c8ebea95c32d600b8ad187ec582954d538b
Wizzy Dev <wizzypump@gmail.com>
2025-11-18 11:18:24 +0100

    Add project files.

---
+++ TakeEase/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TakeEase")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TakeEase.Pages")]
